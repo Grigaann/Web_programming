851fe913d0b04b5a0f3b89a084dc2ba474cb29ea
Shirohige <ylanheb@gmail.com>
2023-05-18 11:40:23 +0200

    js part begun

---
+++ Lab 2&3/js/script.js
@@ -0,0 +1,10 @@
+console.log("I'm the console !");
+
+function welcome(){
+    alert("Your password has not been added :)");
+}
+
+function generate(){
+    var myForm = document.forms.ajoutPWD;
+    console.log(myForm);
+}
